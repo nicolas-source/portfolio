3790bf892e119ac34d9c1af02dbd16fbfa13743d
nicolas-source <nzheng8@student.ubc.ca>
2023-07-22 10:56:02 -0700

    Revert "updated next.config"
    This reverts commit b6caaf5db21f2e53104a7638bf26c88c993aef27.

--- next.config.js
+++ next.config.js
@@ -23,7 +23,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    output: 'export',
     assetPrefix: isProd ? '/portfolio/' : '',
     // assetPrefix: '',
     // assetPrefix: './',
